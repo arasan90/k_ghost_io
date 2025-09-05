2b47c95513c4206be507e3da770f9eaba56b8cb3
Massimiliano Ianniello <massimilianoianniello@gmail.com>
2025-09-05 17:11:06 +0200

    Fix variable used in cmake functions

--- k_ghost_io.cmake
+++ k_ghost_io.cmake
@@ -2,19 +2,19 @@ cmake_minimum_required(VERSION 3.10)
 
 set(sources
     ${CMAKE_CURRENT_LIST_DIR}/src/${TARGET_PLATFORM}/k_ghost_io.c
-    )
+)
 
-set (public_includes
+set(public_includes
     ${CMAKE_CURRENT_LIST_DIR}/include
-    )
+)
 
 set(private_includes
     ${CMAKE_CURRENT_LIST_DIR}/src
-    )
+)
 
 set(public_linked_libs
     k_cjson
-    )
+)
 
 set(private_linked_libs
 )
@@ -50,13 +50,13 @@ function(k_ghost_io_get_public_linked_libs OUT_VAR)
         PARENT_SCOPE)
 endfunction()
 
-function (k_ghost_io_create_mock_library)
-    add_library(k_ghost_io_mock ${CMAKE_CURRENT_LIST_DIR}/mock/k_ghost_io_mock.c)
-    target_include_directories(k_ghost_io_mock PUBLIC ${CMAKE_CURRENT_LIST_DIR}/include)
-    target_include_directories(k_ghost_io_mock PRIVATE ${CMAKE_CURRENT_LIST_DIR}/mock)
+function(k_ghost_io_create_mock_library)
+    add_library(k_ghost_io_mock ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/mock/k_ghost_io_mock.c)
+    target_include_directories(k_ghost_io_mock PUBLIC ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/include)
+    target_include_directories(k_ghost_io_mock PRIVATE ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/mock)
     target_link_libraries(k_ghost_io_mock PUBLIC k_cjson)
 endfunction()
 
 function(k_ghost_io_create_dep_libraries)
-    add_subdirectory(libs/cjson)
+    add_subdirectory(${CMAKE_CURRENT_FUNCTION_LIST_DIR}/libs/cjson)
 endfunction()
